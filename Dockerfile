b4be38f3542174416cee37a84f2e5f2fbb704a9f
bafraikin <fraikin.baptiste@gmail.com>
2018-08-21 19:17:50 +0200

    everthing is broken

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ RUN apt-get update && apt-get install -y xvfb iceweasel
 RUN apt-get install -y ruby bundler git
 RUN gem install selenium-webdriver
 RUN gem install watir
+RUN gem install headless
 
 ADD xvfb-firefox /usr/bin/xvfb-firefox
+ADD geckodriver /usr/bin/geckodriver
 RUN rm /usr/bin/firefox
 RUN ln -s /usr/bin/xvfb-firefox /usr/bin/firefox
 
--- duck.rb
+++ duck.rb
@@ -1,6 +1,27 @@
 require 'watir'
+require 'headless'
 
-browser = Watir::Browser.new :firefox
+=begin
+def get_arguments
+  browser_arguments = { }
+
+  capabilities = Selenium::WebDriver::Remote::Capabilities.firefox()
+
+  capabilities[:name] = 'Watir'
+  browser_arguments[:opt] = capabilities
+
+  browser_arguments
+end
+=end
+
+
+
+headless = Headless.new
+headless.start
+
+browser = Watir::Browser.new(:firefox,
+                             :prefs => {:password_manager_enable => false, :credentials_enable_service => false},
+                             :switches => ["disable-infobars", "no-sandbox"])
 browser.driver.manage.timeouts.implicit_wait = 5
 
 quot_search  = File.open("app/URL", "r")
@@ -16,3 +37,4 @@ quot_search.each_with_index do | line,index |
 end
 
 browser.close
+headless.destroy
